b2db554d9f14d35b1876e69ea1ecefd94bb6e0d0
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-07-25 08:50:17 -0700

    assignmnt complete

---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+  {
+    id: 1,
+    title: "1",
+    url: '"www.myspace.com',
+    rating: "4",
+    description: "This is bookmark item 1"
+  }
+];
+
+module.exports = { bookmarks };

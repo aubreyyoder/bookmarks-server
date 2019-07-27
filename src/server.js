7336393e5f13a31464b91f3a06c8d1bcdaace06f
Aubrey Yoder <Aubreyyoder@gmail.com>
2019-07-27 12:13:42 -0600

    Updated everything

--- src/server.js
+++ src/server.js
@@ -1,7 +1,6 @@
 const app = require("./app");
-
 const { PORT } = require("./config");
 
 app.listen(PORT, () => {
-  console.log(`server listening at http://localhost:${PORT}`);
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
--- src/store.js
+++ src/store.js
@@ -1,10 +1,26 @@
+const uuid = require("uuid/v4");
+
 const bookmarks = [
   {
-    id: 1,
-    title: "1",
-    url: '"www.myspace.com',
-    rating: "4",
-    description: "This is bookmark item 1"
+    id: uuid(),
+    title: "Thinkful",
+    url: "https://www.thinkful.com",
+    description: "Think outside the classroom",
+    rating: 5
+  },
+  {
+    id: uuid(),
+    title: "Google",
+    url: "https://www.google.com",
+    description: "Where we find everything else",
+    rating: 4
+  },
+  {
+    id: uuid(),
+    title: "MDN",
+    url: "https://developer.mozilla.org",
+    description: "The only place to find web documentation",
+    rating: 5
   }
 ];
 
